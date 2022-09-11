1d8fc09490a139e70f68673b52fedb79695c686c
Alejandro Cala <acala016@fiu.edu>
2022-09-11 14:17:52 -0400

    fix

--- assignment2.py
+++ assignment2.py
@@ -9,15 +9,16 @@ class Assignment2:
 
     def listAnniversaries(self):  #task 3
         years = (2022 - self.year)
-        aniversaries = [0]  # list of anniversaries 
+        aniversaries = []  # list of anniversaries 
 
         modYears = years % 10 # years % 10
         modYears1 = years - modYears # years - mod
         limit = modYears1 / 10 # new result / 10 
+       
 
-        for n in range(int(limit)):  # the new result will be somehow placed as 10, 20 , limit to muliply by 10 
-            result = n*10
-            aniversaries = [result]
+        for n in range(1, int(limit)+1):  # the new result will be somehow placed as 10, 20 , limit to muliply by 10 
+            n = n*10
+            aniversaries.append(n)
 
         return aniversaries
 
@@ -25,7 +26,7 @@ class Assignment2:
         subYear = str(n[0:2])
         print(subYear)
 
-obj = Assignment2(1998)  # must input valid year
+obj = Assignment2(1989)  # must input valid year
 obj.tellAge(2022)        # input the  current year
 ret = obj.listAnniversaries()
 print(ret)        
